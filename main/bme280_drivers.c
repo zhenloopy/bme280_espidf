66fa5edd27c2f884155c977a735a86ab8b8ddba3
zhenloopy <145357830+zhenloopy@users.noreply.github.com>
2025-01-28 16:50:42 -0600

    NOT FINISHED got includes working

--- main/bme280_drivers.c
+++ main/bme280_drivers.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include "bme280.h"
+#include "driver/i2c.h"
+
+#define I2C_NUM I2C_NUM_0
+#define I2C_SCL GPIO_NUM_22
+#define I2C_SDA GPIO_NUM_21
+
+/*static esp_err_t i2c_master_init(void) {
+
+	int i2c_master_port = I2C_NUM;
+
+	i2c_config_t conf = {
+		.mode = I2C_MODE_MASTER,
+		.sda_io_num = I2C_SDA,
+        .scl_io_num = I2C_SCL,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 400000,
+    };
+	i2c_param_config(i2c_master_port, &conf);
+	return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+} */
+
 
 void app_main(void)
 {
-
+	while(1) {
+	printf("hi\n\n");
+	}
 }
